ab1016d6989a8f3a1eb4f262b737a16cf409cc90
Giorgi Mukhigulashvili <gmukhigulashvili17@gmail.com>
2018-02-28 20:28:12 +0400

    minor fix

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# UPDATE
 apt-get update
 apt-get upgrade -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -8,3 +10,21 @@ apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo apt-get install -y docker-ce
 sudo apt-get install -y docker-compose
+
+# FORMAT DISK
+fdisk /dev/sdc <<EOF
+n
+p
+1
+
+
+w
+EOF
+
+sudo mkfs -t ext4 /dev/sdc
+
+sudo -i blkid
+
+
+mkdir ~/Docker-Compose
+mkdir ~/Docker-Compose/Mongo
